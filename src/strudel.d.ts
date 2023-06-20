1cc8a383f4a1940e0b17c9b81738bcae4aa9bffd
Roipoussiere <roipoussiere@protonmail.com>
2023-06-20 14:58:06 +0200

    convert to typescript

---
+++ src/strudel.d.ts
@@ -0,0 +1,5 @@
+declare module '@strudel.cycles/core';
+declare module '@strudel.cycles/mini';
+declare module '@strudel.cycles/webaudio';
+declare module '@strudel.cycles/tonal';
+declare module '@strudel.cycles/soundfonts';
--- src/tidaml.ts
+++ src/tidaml.ts
@@ -1,15 +1,16 @@
 import * as yaml from 'js-yaml'
 
+import { EditorView, keymap } from '@codemirror/view';
+import { solarizedLight } from '@uiw/codemirror-theme-solarized';
+import { yaml as yamlMode } from '@codemirror/legacy-modes/mode/yaml';
+import { StreamLanguage, LanguageSupport } from '@codemirror/language'
+
 import * as core from '@strudel.cycles/core';
 import * as mini from '@strudel.cycles/mini';
 import * as webaudio from '@strudel.cycles/webaudio';
 import * as tonal from '@strudel.cycles/tonal';
 import { registerSoundfonts } from '@strudel.cycles/soundfonts';
 
-import { EditorView, keymap } from '@codemirror/view';
-import { solarizedLight } from '@uiw/codemirror-theme-solarized';
-import * as yamlMode from '@codemirror/legacy-modes/mode/yaml';
-import { StreamLanguage, LanguageSupport } from '@codemirror/language'
 
 const TUNES_PATH = './tunes/'
 const TUNES = [ 'ws2_stack', 'ws3_delay', 'ws3_dub_tune', 'ws3_stack_in_stack', 'ws4_add_stack',
@@ -19,10 +20,12 @@ const SIGNALS_FN = [ 'Saw', 'Sine', 'Cosine', 'Tri', 'Square', 'Rand', 'Perlin',
   'Saw2', 'Sine2', 'Cosine2', 'Tri2', 'Square2', 'Rand2' ]
 const LAMBDA_PARAMS_NAME = [ 'a', 'b', 'c']
 
-const ctx = webaudio.getAudioContext();
 
-const yamlLang = new LanguageSupport(StreamLanguage.define(yamlMode.yaml));
-const domSelectTune = document.getElementById('select_tune');
+const domSelectTune = <HTMLSelectElement> document.getElementById('select_tune');
+const domBtnStart = <HTMLInputElement> document.getElementById('start');
+const domBtnStop = <HTMLInputElement> document.getElementById('stop');
+
+const yamlLang = new LanguageSupport(StreamLanguage.define(yamlMode));
 const editor = new EditorView({
   extensions: [
     solarizedLight,
@@ -32,26 +35,12 @@ const editor = new EditorView({
       { key: 'Ctrl-.', run: () => onStop() },
     ])
   ],
-  parent: document.getElementById('input')
+  parent: <HTMLElement> document.getElementById('input')
 });
+
+const ctx = webaudio.getAudioContext();
 
 initAudio();
-
-window.addEventListener('DOMContentLoaded', () => {
-  TUNES.map( tune => {
-    const domTuneItem = document.createElement('option');
-    domTuneItem.value = tune;
-    domTuneItem.innerHTML = tune.replaceAll('_', ' ');
-    domSelectTune.append(domTuneItem);
-  });
-
-  loadTune(window.location.hash.substring(1)
-    || TUNES[Math.floor(Math.random() * TUNES.length)]);
-});
-
-domSelectTune.addEventListener('change', event => {
-  loadTune(event.target.value);
-});
 
 const { evaluate, stop } = core.repl({
   defaultOutput: webaudio.webaudioOutput,
@@ -59,7 +48,14 @@ const { evaluate, stop } = core.repl({
   transpiler
 });
 
-function loadTune(tuneName) {
+window.addEventListener('DOMContentLoaded', onDomLoaded);
+domBtnStart.addEventListener('click', onPlay);
+domBtnStop.addEventListener('click', onStop);
+domSelectTune.addEventListener('change', event => {
+  loadTune((<HTMLSelectElement> event.target).value);
+});
+
+function loadTune(tuneName: string): void {
   console.log(`Loading tune ${tuneName}...`)
   fetch(TUNES_PATH + tuneName + '.yml')
   .then(response => response.text())
@@ -76,10 +72,19 @@ function loadTune(tuneName) {
   window.location.hash = `#${ tuneName }`
 }
 
-document.getElementById('start').addEventListener('click', onPlay);
-document.getElementById('stop').addEventListener('click', onStop);
+function onDomLoaded(): void {
+  TUNES.map( tune => {
+    const domTuneItem = document.createElement('option');
+    domTuneItem.value = tune;
+    domTuneItem.innerHTML = tune.replace(/_/g, ' ');
+    domSelectTune.append(domTuneItem);
+  });
 
-function initAudio() {
+  loadTune(window.location.hash.substring(1)
+    || TUNES[Math.floor(Math.random() * TUNES.length)]);
+}
+
+function initAudio(): void {
   webaudio.samples('https://strudel.tidalcycles.org/vcsl.json',
     'github:sgossner/VCSL/master/', { prebake: true })
   webaudio.samples('https://strudel.tidalcycles.org/piano.json',
@@ -95,30 +100,30 @@ function initAudio() {
   core.evalScope(core.controls, core, mini, webaudio, tonal);  
 }
 
-function transpiler(inputYaml) {
+function transpiler(inputYaml: string): string {
   const tune = yaml.load(inputYaml)
-  let outputJs = readOutro(tune)
-  outputJs += `return ${ readBlock(tune instanceof Array ? { 'Stack': tune } : tune) }\n`
+  let outputJs = parseOutro(tune)
+  outputJs += `return ${ parseNode(tune instanceof Array ? { 'Stack': tune } : tune) }\n`
   console.log(outputJs);
   return outputJs
 }
 
-function readBlock(block, indentLvl=0) {
+function parseNode(node: any, indentLvl=0): string {
   let js
 
-  if (block instanceof Array) {
-    js = block.map(item => readBlock(item, indentLvl + 2)).join(', ')
-  } else if (block instanceof Object) {
-    js = readObject(block, indentLvl)
+  if (node instanceof Array) {
+    js = node.map(item => parseNode(item, indentLvl + 2)).join(', ')
+  } else if (node instanceof Object) {
+    js = parseObject(node, indentLvl)
   } else {
-    js = valueToString(block)
+    js = valueToString(node)
   }
 
   return js
 }
 
-function getMainAttribute(obj) {
-  const mainAttrs = Object.keys(obj).filter(key => key[0] != key[0].toLowerCase())
+function getMainAttribute(node: any): string {
+  const mainAttrs = Object.keys(node).filter(key => key[0] != key[0].toLowerCase())
   let mainAttr = ''
 
   if (mainAttrs.length == 0) {
@@ -131,15 +136,15 @@ function getMainAttribute(obj) {
   return mainAttr
 }
 
-function getOutroAttributes(obj) {
-  return Object.keys(obj).filter(key => key[0] === '.')
+function getOutroAttributes(node: any): string[] {
+  return Object.keys(node).filter(key => key[0] === '.')
 }
 
-function getOtherAttributes(obj) {
-  return Object.keys(obj).filter(key => key[0] === key[0].toLowerCase() && key[0] !== '.')
+function getOtherAttributes(node: any): string[] {
+  return Object.keys(node).filter(key => key[0] === key[0].toLowerCase() && key[0] !== '.')
 }
 
-function valueToString(value) {
+function valueToString(value: any): string {
   if (value === null) {
     value = ''
   } else if ( ! isNaN(value)) {
@@ -156,73 +161,75 @@ function valueToString(value) {
   return value
 }
 
-function indent(lvl) {
+function indent(lvl: number): string {
   return lvl == 0 ? '' : '\n' + '  '.repeat(lvl)
 }
 
-function readOutro(obj) {
+function parseOutro(node: any): string {
   let js = ''
-  for (const attr of getOutroAttributes(obj)) {
+  for (const attr of getOutroAttributes(node)) {
     const newAttr = attr.split('^')[0]
     const prefix = attr[1] === '.' ? `await ${ newAttr.substring(2) }` : newAttr.substring(1)
-    js += `${ prefix }(${ getValue(attr, obj) })\n`
+    js += `${ prefix }(${ getValue(attr, node, 0) })\n`
   }
   return js
 }
 
-function getValue(attr, obj, indentLvl) {
+function getValue(attr: string, node: any, indentLvl: number): string {
   let serialize = attr.split('^')[1]
 
   if (serialize === undefined) {
-    return readBlock(obj[attr], indentLvl)
+    return parseNode(node[attr], indentLvl)
   } else if (serialize === '') {
-      return JSON.stringify(obj[attr], null, '  ').replaceAll('"', "'")
+      return JSON.stringify(node[attr], null, '  ').replace(/_/g, ' ')
   } else {
     const paramId = parseInt(serialize) - 1
-    return obj[attr].map((value, id) =>
+    return node[attr].map((node: any, id: number) =>
       id == paramId ?
-        JSON.stringify(value, null, '  ').replaceAll('"', "'")
-      : valueToString(value))
+        JSON.stringify(node, null, '  ').replace(/_/g, ' ')
+      : valueToString(node))
   }
 }
 
-function readObject(obj, indentLvl) {
+function parseObject(node: any, indentLvl: number): string {
   let js, value
-  const mainAttr = getMainAttribute(obj)
+  const mainAttr = getMainAttribute(node)
 
   if (mainAttr === 'M') {
-    js = valueToString(obj[mainAttr])
+    js = valueToString(node[mainAttr])
   } else if (mainAttr === 'Set') {
-    if (obj[mainAttr] === null) {
+    if (node[mainAttr] === null) {
       js = 'x => x'
     } else {
-      const param_names = LAMBDA_PARAMS_NAME.slice(0, obj[mainAttr])
+      const param_names = LAMBDA_PARAMS_NAME.slice(0, node[mainAttr])
       js = `(x, ${ param_names.join(', ') }) => x`
     }
     indentLvl -= 1
   } else {
     const newAttr = mainAttr.split('^')[0]
     js = indent(indentLvl) + newAttr[0].toLowerCase() + newAttr.substring(1)
-    if (obj[mainAttr] !== null && ! SIGNALS_FN.includes(newAttr)) {
-      value = getValue(mainAttr, obj, indentLvl)
+    if (node[mainAttr] !== null && ! SIGNALS_FN.includes(newAttr)) {
+      value = getValue(mainAttr, node, indentLvl)
       js += `(${ value })`
     }
   }
 
-  for (let attr of getOtherAttributes(obj)) {
+  for (let attr of getOtherAttributes(node)) {
     const newAttr = attr.split('^')[0]
-    value = getValue(attr, obj, indentLvl)
+    value = getValue(attr, node, indentLvl)
     js += indent(indentLvl + 1) + `.${ newAttr }(${ value })`
   }
 
   return js;
 }
 
-function onPlay() {
+function onPlay(): boolean {
   ctx.resume();
   evaluate(editor.contentDOM.innerText);
+  return false;
 }
 
-function onStop() {
+function onStop(): boolean {
   stop();
+  return false;
 }
