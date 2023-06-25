c7ca4f59b3d9a1171e2307f8586f89278899173d
Roipoussiere <roipoussiere@protonmail.com>
2023-06-25 12:28:08 +0200

    update jest config file, use jsdom env

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
+// Jest configuration file - see https://jestjs.io/docs/configuration
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+	clearMocks: true,
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageProvider: 'v8',
+	errorOnDeprecated: true,
+	maxWorkers: '100%',
+	notify: false,
+	notifyMode: 'failure-change',
 	preset: 'ts-jest',
-	testEnvironment: 'node',
+	slowTestThreshold: 1,
+	testEnvironment: 'jsdom',
+	testMatch: ['**/tests/*.test.ts'],
 };
