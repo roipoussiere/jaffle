e622d719048b3742d249b61b665dbdfe627c53a4
Roipoussiere <roipoussiere@protonmail.com>
2023-06-26 17:13:03 +0200

    simplify error messages

--- src/errors.ts
+++ src/errors.ts
@@ -2,42 +2,42 @@
 
 export class JaffleError extends Error {
 	constructor(message: string) {
-		super(`Jaffle error: ${message}.`);
+		super(`${message}.`);
 		this.name = JaffleError.name;
 	}
 }
 
 export class JaffleErrorBadYaml extends JaffleError {
 	constructor(message: string) {
-		super(`bad yaml syntax: ${message}`);
+		super(message);
 		this.name = JaffleErrorBadYaml.name;
 	}
 }
 
 export class JaffleErrorNotImplemented extends JaffleError {
-	constructor() {
-		super('not implemented yet');
+	constructor(message: string) {
+		super(message);
 		this.name = JaffleErrorNotImplemented.name;
 	}
 }
 
 export class JaffleErrorBadType extends JaffleError {
 	constructor(expecting: string, got: string) {
-		super(`bad type: expecting ${expecting} but got ${got}`);
+		super(`expecting ${expecting} but got ${got}`);
 		this.name = JaffleErrorBadType.name;
 	}
 }
 
 export class JaffleAttributeError extends JaffleError {
 	constructor(message: string) {
-		super(`attribute error: ${message}`);
+		super(message);
 		this.name = JaffleAttributeError.name;
 	}
 }
 
 export class JaffleErrorMainAttr extends JaffleError {
 	constructor(message: string) {
-		super(`main attribute ${message}`);
+		super(message);
 		this.name = JaffleErrorMainAttr.name;
 	}
 }
