5f3357c127e15b0766a3b7287723395e3ec4ab9d
Roipoussiere <roipoussiere@protonmail.com>
2023-06-27 15:14:11 +0200

    add errors tests

--- tests/transpiler.test.ts
+++ tests/transpiler.test.ts
@@ -265,3 +265,16 @@ note:
 		expect(t.jaffleDocumentToJs('{Init: [], Foo: 42}')).toBe('return foo(42);');
 	});
 });
+
+describe('Testing errors', () => {
+	test('Errors should throw when throwed', () => {
+		expect(() => { throw new e.JaffleError(''); }).toThrow(e.JaffleError);
+		expect(() => { throw new e.NotImplementedJaffleError(''); }).toThrow(e.NotImplementedJaffleError);
+		expect(() => { throw new e.BadYamlJaffleError(''); }).toThrow(e.BadYamlJaffleError);
+		expect(() => { throw new e.BadTypeJaffleError('a', 'b'); }).toThrow(e.BadTypeJaffleError);
+		expect(() => { throw new e.BadFunctionJaffleError(''); }).toThrow(e.BadFunctionJaffleError);
+		expect(() => { throw new e.BadListJaffleError(''); }).toThrow(e.BadListJaffleError);
+		expect(() => { throw new e.BadInitBlockJaffleError(''); }).toThrow(e.BadInitBlockJaffleError);
+		expect(() => { throw new e.BadDocumentJaffleError(''); }).toThrow(e.BadDocumentJaffleError);
+	});
+});
