6b2c46cf669d7b939884b28fa07d182a77807145
Roipoussiere <roipoussiere@protonmail.com>
2023-07-07 19:01:52 +0200

    graph poc: fit box size not char size

--- src/graph.ts
+++ src/graph.ts
@@ -30,48 +30,52 @@ class JaffleGraph {
 	public draw(): void {
 		const width = 800;
 		const height = 400;
-		const boxWidth = 8;
-		const boxSpacing = 10;
+		const fontSize = 16;
+		const boxWidth = 20;
+		const boxGap = 3;
+
+		const charWidth = fontSize * 0.6;
+		const charHeight = fontSize * 1.5;
+		const boxSpacing = boxWidth + boxGap;
 		const root = d3.hierarchy(this.data, (d: any) => d.children); // TODO: add more logic
 
+		const scaleX = (d: any) => (d.depth - 1) * boxSpacing * charWidth;
+		const scaleY = (d: any) => d.data.y * charHeight;
+
 		const svg = d3.create('svg')
+			.attr('class', 'jaffle_graph')
 			.attr('width', width)
 			.attr('height', height)
-			.attr('viewBox', [0, 0, 40, 20])
-			.attr('style', 'max-width: 100%; height: auto; font: 0.8px mono;');
+			.attr('viewBox', [0, 0, width, height])
+			.attr('style', `max-width: 100%; height: auto; font: ${fontSize}px mono;`);
 
 		svg.append('g') // link
 			.attr('fill', 'none')
 			.attr('stroke', 'black')
 			.attr('stroke-opacity', 0.5)
-			.attr('stroke-width', 0.1)
+			.attr('stroke-width', 2)
 			.selectAll()
 			.data(root.links()
 				.filter((d: any) => d.source.depth >= 1 && d.target.data.name[0] !== '.'))
 			.join('path')
-			.attr('d', d3.linkHorizontal()
-				.x((d: any) => (d.depth - 1) * boxSpacing)
-				.y((d: any) => d.data.y));
-		// .attr('transform', (d: any) => `scale(${0.5})`);
+			.attr('d', d3.linkHorizontal().x(scaleX).y(scaleY));
 
 		const node = svg.append('g')
-			.attr('stroke-linejoin', 'round')
-			.attr('stroke-width', 0.2)
 			.selectAll()
 			.data(root.descendants().filter((d: any) => d.depth >= 1))
 			.join('g')
-			.attr('transform', (d: any) => `translate(${(d.depth - 1) * boxSpacing},${d.data.y})`);
+			.attr('transform', (d: any) => `translate(${scaleX(d)},${scaleY(d)})`);
 
 		node.append('rect')
-			.attr('width', boxWidth)
-			.attr('height', 1)
-			.attr('y', -0.5)
-			.attr('rx', 0.2)
-			.attr('ry', 0.2)
+			.attr('width', boxWidth * charWidth)
+			.attr('height', 1 * charHeight)
+			.attr('y', -0.5 * charHeight)
+			.attr('rx', 3)
+			.attr('ry', 3)
 			.attr('fill', '#ccc');
 
 		node.append('text')
-			.attr('y', 0.3)
+			.attr('y', 0.27 * charHeight)
 			.text((d: any) => `${d.data.name}: ${d.data.value || ''}`);
 
 		this.svg = <SVGElement>svg.node();
