5da6c29cc74f9bcebcbc18e6e85a135f9281b78f
Roipoussiere <roipoussiere@protonmail.com>
2023-07-08 12:34:45 +0200

    graph poc: fix links

--- src/graph.ts
+++ src/graph.ts
@@ -23,48 +23,51 @@ class JaffleGraph {
 			throw new errors.BadYamlJaffleError(err.message);
 		}
 		this.rawData = tune;
-
-		this.organize();
 	}
 
 	public draw(): void {
 		const width = 800;
 		const height = 400;
-		const fontSize = 16;
+		const fontSize = 14;
 		const boxWidth = 20;
 		const boxGap = 3;
 
 		const charWidth = fontSize * 0.6;
-		const charHeight = fontSize * 1.5;
+		const charHeight = fontSize * 1.4;
 		const boxSpacing = boxWidth + boxGap;
-		const root = d3.hierarchy(this.data, (d: any) => d.children); // TODO: add more logic
 
-		const scaleX = (d: any) => (d.depth - 1) * boxSpacing * charWidth;
-		const scaleY = (d: any) => d.data.y * charHeight;
+		// TODO: convert rawData to data
+		const root = d3.hierarchy(tuneData, (d: any) => d.children);
+		const tree = d3.tree()
+			.nodeSize([charHeight, boxSpacing * charWidth])
+			.separation((a: any) => (a.data.name[0] === '.' ? 1 : 2));
+
+		tree(root);
 
 		const svg = d3.create('svg')
 			.attr('class', 'jaffle_graph')
 			.attr('width', width)
 			.attr('height', height)
-			.attr('viewBox', [0, 0, width, height])
-			.attr('style', `max-width: 100%; height: auto; font: ${fontSize}px mono;`);
+			.attr('viewBox', [22 * charWidth, -7 * charHeight, width, height])
+			.style('font', `${fontSize}px mono`);
 
 		svg.append('g') // link
 			.attr('fill', 'none')
-			.attr('stroke', 'black')
-			.attr('stroke-opacity', 0.5)
+			.attr('stroke', '#333')
 			.attr('stroke-width', 2)
 			.selectAll()
 			.data(root.links()
 				.filter((d: any) => d.source.depth >= 1 && d.target.data.name[0] !== '.'))
 			.join('path')
-			.attr('d', d3.linkHorizontal().x(scaleX).y(scaleY));
+			.attr('d', (link: any) => d3.linkHorizontal()
+				.x((d: any) => (d.y === link.source.y ? d.y + boxWidth * charWidth : d.y))
+				.y((d: any) => d.x)(link));
 
 		const node = svg.append('g')
 			.selectAll()
 			.data(root.descendants().filter((d: any) => d.depth >= 1))
 			.join('g')
-			.attr('transform', (d: any) => `translate(${scaleX(d)},${scaleY(d)})`);
+			.attr('transform', (d: any) => `translate(${d.y},${d.x})`);
 
 		node.append('rect')
 			.attr('width', boxWidth * charWidth)
@@ -79,11 +82,6 @@ class JaffleGraph {
 			.text((d: any) => `${d.data.name}: ${d.data.value || ''}`);
 
 		this.svg = <SVGElement>svg.node();
-	}
-
-	private organize(): void {
-		// TODO: convert rawData to data
-		this.data = tuneData;
 	}
 }
 
