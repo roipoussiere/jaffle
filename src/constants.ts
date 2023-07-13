9c899dfc9431ff08d09b00dc4d897c837901cd60
Roipoussiere <roipoussiere@protonmail.com>
2023-07-13 22:38:57 +0200

    add file dedicated to constant definitions

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const CHAINED_FUNC_PREFIX = '.';
+export const CONST_FUNC_PREFIX = '$';
+export const SERIALIZE_FUNC_SUFFIX = '^';
+
+export const MINI_STR_PREFIX = '_';
+export const EXPR_STR_PREFIX = '=';
