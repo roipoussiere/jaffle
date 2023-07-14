b3fb9a1782aa654ded77f7890748e21678a7cd42
Roipoussiere <roipoussiere@protonmail.com>
2023-07-14 11:01:45 +0200

    update errors tests

--- tests/errors.test.ts
+++ tests/errors.test.ts
@@ -5,8 +5,11 @@ import * as e from '../src/errors';
 describe('Testing errors', () => {
 	test('Errors should throw when throwed', () => {
 		expect(() => { throw new e.JaffleError(''); }).toThrow(e.JaffleError);
-		expect(() => { throw new e.NotImplementedJaffleError(''); })
-			.toThrow(e.NotImplementedJaffleError);
+		expect(() => { throw new e.NotImplementedError(''); }).toThrow(e.NotImplementedError);
+		expect(() => { throw new e.AbstractClassError(''); }).toThrow(e.AbstractClassError);
+		expect(() => { throw new e.ImporterError(''); }).toThrow(e.ImporterError);
+		expect(() => { throw new e.ExporterError(''); }).toThrow(e.ExporterError);
+
 		expect(() => { throw new e.BadYamlJaffleError(''); }).toThrow(e.BadYamlJaffleError);
 		expect(() => { throw new e.BadStringJaffleError(''); }).toThrow(e.BadStringJaffleError);
 		expect(() => { throw new e.BadFunctionJaffleError(''); }).toThrow(e.BadFunctionJaffleError);
