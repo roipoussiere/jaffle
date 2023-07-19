f91df3495eb66359bc5c035a6141ced7acc887c1
Roipoussiere <roipoussiere@protonmail.com>
2023-07-19 21:27:04 +0200

    add YamlExporter draft

--- src/exporters/yamlExporter.ts
+++ src/exporters/yamlExporter.ts
@@ -1,12 +1,23 @@
-import { NotImplementedError } from '../errors';
-import { FuncTree } from '../funcTree';
+import { dump as dumpYaml } from 'js-yaml';
+
+import { FuncTree, ValueType } from '../funcTree';
 
 import AbstractExporter from './abstractExporter';
 
+interface Dict<T> {
+	[key: string]: T;
+}
+
 class YamlExporter extends AbstractExporter {
-	// eslint-disable-next-line class-methods-use-this, @typescript-eslint/no-unused-vars
-	public export(composition: FuncTree): string {
-		throw new NotImplementedError('YamlExporter.export()');
+	static export(composition: FuncTree): string {
+		const yamlTree = YamlExporter.arrangeFunc(composition);
+		return dumpYaml(yamlTree);
+	}
+
+	static arrangeFunc(funcTree: FuncTree): Dict<unknown> {
+		const value = funcTree.valueType === ValueType.Tree
+			? funcTree.params.map((param) => YamlExporter.arrangeFunc(param)) : funcTree.value;
+		return { [funcTree.name]: value };
 	}
 }
 
