f2011495eb363d08236c0bbca044745706bf4168
Roipoussiere <roipoussiere@protonmail.com>
2023-07-22 19:22:02 +0200

    remove importer/exporter interfaces, will use boxHelper instead

--- src/exporters/exporterInterface.ts
+++
@@ -1,7 +0,0 @@
-import { Vertex } from '../dataTypes/vertex';
-
-interface Exporter {
-	export: (composition: Vertex) => unknown;
-}
-
-export default Exporter;
--- src/importers/importerInterface.ts
+++
@@ -1,7 +0,0 @@
-import { Vertex } from '../dataTypes/vertex';
-
-interface Importer {
-	import: (thing: unknown) => Vertex;
-}
-
-export default Importer;
