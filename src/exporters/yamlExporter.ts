4cdbd36a70b87347948facbad66a1f95ad0f2315
Roipoussiere <roipoussiere@protonmail.com>
2023-07-24 10:04:31 +0200

    update yamlExporter draft

--- src/exporters/yamlExporter.ts
+++ src/exporters/yamlExporter.ts
@@ -1,10 +1,18 @@
 // eslint-disable-next-line max-classes-per-file
 import { dump as dumpYaml } from 'js-yaml';
 
-import { Entry } from '../model';
+import { Dict, Entry } from '../model';
+
+export function entryToObject(entry: Entry): Dict<unknown> {
+	return {
+		[entry.rawName]: entry.children.length === 0
+			? entry.rawValue : entry.children.map((child) => entryToObject(child)),
+	};
+}
 
 export function entryToYaml(entry: Entry): string {
-	return dumpYaml(entry);
+	const object = entryToObject(entry);
+	return dumpYaml(object);
 }
 
 export default entryToYaml;
