602a130406610dcc58fd9a3aa081a7e069825d3a
Roipoussiere <roipoussiere@protonmail.com>
2023-07-24 10:41:06 +0200

    update astNodeExporter draft

--- src/exporters/astNodeExporter.ts
+++ src/exporters/astNodeExporter.ts
@@ -1,39 +1,46 @@
-import { AstNode, BoxType, Entry, ValueType } from '../model';
+import { AstNode, BoxType, Entry } from '../model';
+import * as c from '../constants';
 
-export function getBoxType(vBox: Box): BoxType {
-	let funcType: BoxType;
-	if (vBox.rawName[0] === '$') {
-		funcType = BoxType.ConstantDef;
-	} else if (vBox.rawName[0] === '.') {
-		funcType = BoxType.ChainedFunc;
-	} else if (vBox.rawName.slice(-1) === '^') {
-		funcType = BoxType.SerializedData;
-	// } else if (boxTree.children.length > 0) {
-	// 	funcType = VertexType.List;
-	} else if (vBox.rawName === '') {
-		funcType = BoxType.Value;
+export function getBoxType(rawName: string): BoxType {
+	const prefix = rawName[0];
+	let boxType: BoxType;
+	if (prefix === c.CHAINED_FUNC_PREFIX) {
+		boxType = BoxType.ChainedFunc;
+	} else if (prefix === c.CONST_FUNC_PREFIX) {
+		boxType = BoxType.ConstantDef;
+	} else if (rawName.slice(-1) === c.SERIALIZE_FUNC_SUFFIX) {
+		boxType = BoxType.SerializedData;
+	} else if (rawName === '') {
+		boxType = BoxType.Value;
 	} else {
-		funcType = BoxType.MainFunc;
+		boxType = BoxType.MainFunc;
 	}
-	return funcType;
+	return boxType;
 }
 
-export default function entryToAstNode(entry: Entry): AstNode {
-	if (entry.type === BoxType.Value) {
-		return {
-			rawName: '',
-			type: BoxType.Value,
-			rawValue: `${entry.value}`,
-			valueType: ValueType.String,
-			children: [],
-		};
-	}
+export function getValue(rawValue: string): unknown {
+	let value: unknown;
 
+	if (rawValue === '') {
+		value = null;
+	} else if (!Number.isNaN(rawValue)) {
+		value = Number(rawValue);
+	} else if (rawValue === 'true') {
+		value = true;
+	} else if (rawValue === 'false') {
+		value = false;
+	} else {
+		value = rawValue;
+	}
+	return value;
+}
+
+export function entryToAstNode(entry: Entry): AstNode {
 	return {
-		rawName: `${entry.value}`, // TODO
-		type: BoxType.MainFunc,
-		rawValue: `${entry.value}`, // TODO
-		valueType: ValueType.String,
+		type: getBoxType(entry.rawName),
+		value: getValue(entry.rawValue),
 		children: entry.children.map((child) => entryToAstNode(child)),
 	};
 }
+
+export default entryToAstNode;
