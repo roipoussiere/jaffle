69ddffe907b887e8051b4c340378f00fe7ddb405
Roipoussiere <roipoussiere@protonmail.com>
2023-07-24 13:32:49 +0200

    fix BoxImporter tests

--- tests/importers/boxImporter.test.ts
+++ tests/importers/boxImporter.test.ts
@@ -1,137 +1,72 @@
 import { describe, expect, test } from '@jest/globals';
 
-import * as BTI from '../../src/importers/boxTreeImporter';
-import { Box, BoxType, BoxValueType } from '../../src/dataTypes/box';
-import { Vertex, VertexType } from '../../src/dataTypes/vertex';
+import * as BI from '../../src/importers/boxImporter';
+import { Box, BoxType, Entry, ValueType } from '../../src/model';
 
-describe('Testing BTI.boxToVertex()', () => {
-	test('box of literal value can be converted to a vertex', () => {
+describe('Testing BI.boxToEntry()', () => {
+	test('box without children can be converted to an Entry', () => {
 		const input: Box = {
-			name: '',
-			type: BoxType.Value,
-			value: 'a',
-			valueType: BoxValueType.String,
-			children: [],
-		};
-
-		const expected: Vertex = {
-			type: VertexType.Literal,
-			value: 'a',
-			children: [],
-		};
-
-		expect(BTI.boxToVertex(input)).toEqual(expected);
-	});
-
-	test('box of mininotation value can be converted to a vertex', () => {
-		const input: Box = {
-			name: '',
-			type: BoxType.Value,
-			value: 'a',
-			valueType: BoxValueType.Mininotation,
-			children: [],
-		};
-
-		const expected: Vertex = {
-			type: VertexType.MainFunc,
-			value: '_mini',
-			children: [{
-				type: VertexType.Literal,
-				value: 'a',
-				children: [],
-			}],
-		};
-
-		expect(BTI.boxToVertex(input)).toEqual(expected);
-	});
-
-	test('box of expression value can be converted to a vertex', () => {
-		const input: Box = {
-			name: '',
-			type: BoxType.Value,
-			value: 'a',
-			valueType: BoxValueType.Expression,
-			children: [],
-		};
-
-		const expected: Vertex = {
-			type: VertexType.MainFunc,
-			value: '_expr',
-			children: [{
-				type: VertexType.Literal,
-				value: 'a',
-				children: [],
-			}],
-		};
-
-		expect(BTI.boxToVertex(input)).toEqual(expected);
-	});
-
-	test('box of main func with literal can be converted to a vertex', () => {
-		const input: Box = {
-			name: 'a',
+			rawName: 'a',
+			rawValue: 'b',
 			type: BoxType.MainFunc,
-			value: 'b',
-			valueType: BoxValueType.String,
+			valueType: ValueType.String,
+			id: '',
+			groupId: 0,
+			displayName: 'a',
+			displayValue: 'b',
+			padding: 2,
+			width: 3,
 			children: [],
 		};
 
-		const expected: Vertex = {
-			type: VertexType.MainFunc,
-			value: 'a',
-			children: [{
-				type: VertexType.Literal,
-				value: 'b',
-				children: [],
-			}],
+		const expected: Entry = {
+			rawName: 'a',
+			rawValue: 'b',
+			children: [],
 		};
 
-		expect(BTI.boxToVertex(input)).toEqual(expected);
+		expect(BI.boxToEntry(input)).toEqual(expected);
 	});
 
-	test('box of main func with params can be converted to a vertex', () => {
-		const input: Box = {
-			name: 'a',
+	test('box with children can be converted to an Entry', () => {
+		const child: Box = {
+			rawName: 'b',
+			rawValue: 'c',
 			type: BoxType.MainFunc,
-			value: null,
-			valueType: BoxValueType.Empty,
+			valueType: ValueType.String,
+			id: '0',
+			groupId: 0,
+			displayName: 'b',
+			displayValue: 'c',
+			padding: 2,
+			width: 3,
+			children: [],
+		};
+
+		const input: Box = {
+			rawName: 'a',
+			rawValue: '',
+			type: BoxType.MainFunc,
+			valueType: ValueType.String,
+			id: '',
+			groupId: 0,
+			displayName: 'a',
+			displayValue: '',
+			padding: 2,
+			width: 3,
+			children: [child],
+		};
+
+		const expected: Entry = {
+			rawName: 'a',
+			rawValue: '',
 			children: [{
-				name: 'b',
-				type: BoxType.MainFunc,
-				value: true,
-				valueType: BoxValueType.Boolean,
-				children: [],
-			}, {
-				name: '.c',
-				type: BoxType.ChainedFunc,
-				value: 42,
-				valueType: BoxValueType.Number,
+				rawName: 'b',
+				rawValue: 'c',
 				children: [],
 			}],
 		};
 
-		const expected: Vertex = {
-			type: VertexType.MainFunc,
-			value: 'a',
-			children: [{
-				type: VertexType.MainFunc,
-				value: 'b',
-				children: [{
-					type: VertexType.Literal,
-					value: true,
-					children: [],
-				}],
-			}, {
-				type: VertexType.ChainedFunc,
-				value: '.c',
-				children: [{
-					type: VertexType.Literal,
-					value: 42,
-					children: [],
-				}],
-			}],
-		};
-
-		expect(BTI.boxToVertex(input)).toEqual(expected);
+		expect(BI.boxToEntry(input)).toEqual(expected);
 	});
 });
