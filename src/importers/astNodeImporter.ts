ff3aae45d5b7ba8d049c2ca147d6625f743aed16
Roipoussiere <roipoussiere@protonmail.com>
2023-07-24 10:32:19 +0200

    update astNodeImporter draft

--- src/importers/astNodeImporter.ts
+++ src/importers/astNodeImporter.ts
@@ -1,90 +1,32 @@
-import { AstNode, Entry, BoxType, ValueType } from '../model';
+import { AstNode, Entry, BoxType } from '../model';
 import * as c from '../constants';
 
-// function arrangeTree(vertex: Vertex): DraftBox {
-// 	if (vertex.type === VertexType.Literal) {
-// 		return {
-// 			funcName: '',
-// 			funcType: VertexType.Literal,
-// 			value: vertex.value,
-// 			valueType: VertexType.Literal,
-// 			children: [],
-// 		};
-// 	}
+import { AstNodeImporterError } from './importerErrors';
 
-// 	if (typeof vertex.value !== 'string') {
-// 		throw new GraphExporterError(`Vertex value "${vertex.value}" must be a string`);
-// 	}
-
-// 	if (vertex.children.length === 1 && vertex.children[0].children.length === 0) {
-// 		return {
-// 			funcName: vertex.value,
-// 			funcType: vertex.type,
-// 			value: vertex.children[0].value,
-// 			valueType: vertex.children[0].type,
-// 			children: [],
-// 		};
-// 	}
-
-// 	return {
-// 		funcName: vertex.value,
-// 		funcType: vertex.type,
-// 		value: vertex.children[0].value,
-// 		valueType: vertex.children[0].type,
-// 		children: vertex.children.map((child) => arrangeTree(child)),
-// 	};
-// }
-
-// TODO
-export function astNodeToEntry(astNode: AstNode): Entry {
-	let vertex: Entry;
-
+export function getRawName(astNode: AstNode): string {
+	let rawName: string;
 	if (astNode.type === BoxType.Value) {
-		if (astNode.valueType === ValueType.Mininotation) {
-			vertex = {
-				type: BoxType.MainFunc,
-				value: c.MININOTATION_FUNC_NAME,
-				children: [{
-					type: BoxType.Value,
-					value: astNode.rawValue,
-					children: [],
-				}],
-			};
-		} else if (astNode.valueType === ValueType.Expression) {
-			vertex = {
-				type: BoxType.MainFunc,
-				value: c.EXPRESSION_FUNC_NAME,
-				children: [{
-					type: BoxType.Value,
-					value: astNode.rawValue,
-					children: [],
-				}],
-			};
-		} else {
-			vertex = {
-				type: BoxType.Value,
-				value: astNode.rawValue,
-				children: [],
-			};
-		}
-	} else if (astNode.children.length === 0) {
-		vertex = {
-			type: astNode.type,
-			value: astNode.rawName,
-			children: [{
-				type: BoxType.Value,
-				value: astNode.rawValue,
-				children: [],
-			}],
-		};
+		rawName = '';
+	} else if (astNode.type === BoxType.MainFunc) {
+		rawName = `${astNode.value}`;
+	} else if (astNode.type === BoxType.ChainedFunc) {
+		rawName = `${c.CHAINED_FUNC_PREFIX}${astNode.value}`;
+	} else if (astNode.type === BoxType.ConstantDef) {
+		rawName = `${c.CONST_FUNC_PREFIX}${astNode.value}`;
+	} else if (astNode.type === BoxType.SerializedData) {
+		rawName = `${astNode.value}${c.SERIALIZE_FUNC_SUFFIX}`;
 	} else {
-		vertex = {
-			type: astNode.type,
-			value: astNode.rawName,
-			children: astNode.children.map((child) => astNodeToEntry(child)),
-		};
+		throw new AstNodeImporterError(`Unsupported ast node type: ${BoxType[astNode.type]}`);
 	}
-	return vertex;
+	return rawName;
+}
+
+export function astNodeToEntry(astNode: AstNode): Entry {
+	return {
+		rawName: getRawName(astNode),
+		rawValue: `${astNode.value}`,
+		children: astNode.children.map((child) => astNodeToEntry(child)),
+	};
 }
 
 export default astNodeToEntry;
