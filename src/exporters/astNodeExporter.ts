842ab7b05beea0655e8ff0771add771cb0d6a018
Roipoussiere <roipoussiere@protonmail.com>
2023-07-24 09:34:26 +0200

    move getBoxType from boxImporter

--- src/exporters/astNodeExporter.ts
+++ src/exporters/astNodeExporter.ts
@@ -1,5 +1,23 @@
 import { AstNode, BoxType, Entry, ValueType } from '../model';
 
+export function getBoxType(vBox: Box): BoxType {
+	let funcType: BoxType;
+	if (vBox.rawName[0] === '$') {
+		funcType = BoxType.ConstantDef;
+	} else if (vBox.rawName[0] === '.') {
+		funcType = BoxType.ChainedFunc;
+	} else if (vBox.rawName.slice(-1) === '^') {
+		funcType = BoxType.SerializedData;
+	// } else if (boxTree.children.length > 0) {
+	// 	funcType = VertexType.List;
+	} else if (vBox.rawName === '') {
+		funcType = BoxType.Value;
+	} else {
+		funcType = BoxType.MainFunc;
+	}
+	return funcType;
+}
+
 export default function entryToAstNode(entry: Entry): AstNode {
 	if (entry.type === BoxType.Value) {
 		return {
--- src/importers/boxImporter.ts
+++ src/importers/boxImporter.ts
@@ -1,29 +1,9 @@
-import { Box, Entry, BoxType, ValueType } from '../model';
-
-export function getBoxType(vBox: Box): BoxType {
-	let funcType: BoxType;
-	if (vBox.rawName[0] === '$') {
-		funcType = BoxType.ConstantDef;
-	} else if (vBox.rawName[0] === '.') {
-		funcType = BoxType.ChainedFunc;
-	} else if (vBox.rawName.slice(-1) === '^') {
-		funcType = BoxType.SerializedData;
-	// } else if (boxTree.children.length > 0) {
-	// 	funcType = VertexType.List;
-	} else if (vBox.rawName === '') {
-		funcType = BoxType.Value;
-	} else {
-		funcType = BoxType.MainFunc;
-	}
-	return funcType;
-}
+import { Box, Entry } from '../model';
 
 export function boxToEntry(box: Box): Entry {
 	return {
 		rawName: box.rawName,
-		type: getBoxType(box),
 		rawValue: box.rawValue,
-		valueType: ValueType.String,
 		children: box.children.map((child) => boxToEntry(child)),
 	};
 }
