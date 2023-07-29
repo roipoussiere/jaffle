e0d19ef659c7cfeb437e26875643bcc9b2549762
Roipoussiere <roipoussiere@protonmail.com>
2023-07-30 00:01:35 +0200

    jsExporter: bug fixes and refactoring

--- src/exporters/jsExporter.ts
+++ src/exporters/jsExporter.ts
@@ -32,6 +32,9 @@ export function rawNameToFuncName(rawName: string): string {
 	if (rawName.slice(-1) === c.SERIALIZE_FUNC_SUFFIX) {
 		return rawName.substring(0, rawName.length - 1);
 	}
+	if (rawName[0] === rawName[0].toUpperCase()) {
+		return rawName[0].toLowerCase() + rawName.substring(1);
+	}
 	return rawName;
 }
 
@@ -41,24 +44,22 @@ export function rawNameToFuncName(rawName: string): string {
  * @returns a string of Javascript code coming from the string
  */
 export function rawValueToJs(rawValue: string): string {
-	const isPrefixed = rawValue[0] === c.MINI_STR_PREFIX || rawValue[0] === c.EXPR_STR_PREFIX;
-	const newStr = (isPrefixed ? rawValue.substring(1) : rawValue).trim();
-	const quote = newStr.includes('\n') ? '`' : "'";
-	let js: string;
+	const quote = rawValue.includes('\n') ? '`' : "'";
 
 	if (!Number.isNaN(Number(rawValue)) || rawValue === ''
 		|| rawValue === 'true' || rawValue === 'false') {
-		js = rawValue;
-	} else if (rawValue[0] === c.EXPR_STR_PREFIX) {
-		if (newStr.match(/[^a-zA-Z0-9.+\-*/() ]/g)) {
-			throw new ExporterError(`Not a valid expression string: ${newStr}`);
-		}
-		js = newStr.replace(/[a-z][a-zA-Z0-9]*/g, `${c.VAR_NAME_PREFIX}$&`);
-	} else {
-		js = `${quote}${newStr}${quote}`;
-		js = rawValue[0] === c.MINI_STR_PREFIX ? `mini(${js})` : js;
+		return rawValue;
 	}
-	return js;
+
+	if (rawValue[0] === c.EXPR_STR_PREFIX) {
+		const newStr = rawValue.substring(1).trim();
+		if (newStr.match(/[^a-zA-Z0-9.+\-*/() ]/g)) {
+			throw new ExporterError(`Not a valid expression string: ${rawValue}`);
+		}
+		return newStr.replace(/[a-z][a-zA-Z0-9]*/g, `${c.VAR_NAME_PREFIX}$&`);
+	}
+
+	return `${quote}${rawValue}${quote}`;
 }
 
 /**
@@ -113,29 +114,47 @@ export function entryToEntryType(entry: Entry): EntryType {
  * @param entry The Jaffle function to convert
  * @returns a string of JavaScript code which calls the function
  */
-export function functionEntryToJs(entry: Entry): string {
-	const funcName = rawNameToFuncName(entry.rawName);
-	let js: string;
-	const isVarDef = entry.rawName[0] === c.CONSTANT_DEF_PREFIX;
+export function entryToJs(entry: Entry): string {
+	const entryType = entryToEntryType(entry);
 
-	if (entry.rawName[0] === entry.rawName[0].toUpperCase()) {
-		js = entry.rawName[0].toLowerCase() + entry.rawName.substring(1);
-	} else if (entry.rawValue === '') {
-		js = `${funcName}()`;
-	} else if (entry.rawName === c.LAMBDA_NAME) {
-		js = lambdaEntryToJs(entry.rawValue.split(','));
-	} else {
-		const serializSuffix = entry.rawName.split(c.SERIALIZE_FUNC_SUFFIX)[1];
-		// eslint-disable-next-line no-use-before-define
-		const paramsJs = paramsToJsGroups(entry.children, serializSuffix).join(', ');
-		if (isVarDef) {
-			js = `const ${c.VAR_NAME_PREFIX}${funcName} = ${paramsJs}`;
-		} else {
-			js = `${funcName}(${paramsJs})`;
-		}
+	if (entryType === EntryType.Value) {
+		return rawValueToJs(entry.rawValue);
 	}
 
-	return js;
+	if (entryType === EntryType.MininotationFunction) {
+		return `mini(${entry.rawName.substring(1)})`;
+	}
+
+	if (entryType === EntryType.List) {
+		return `[${entry.children.map((item) => entryToJs(item)).join(', ')}]`;
+	}
+
+	const funcName = rawNameToFuncName(entry.rawName);
+
+	if (entryType === EntryType.Object) {
+		return funcName;
+	}
+
+	if (entryType === EntryType.LambdaFunction) {
+		return lambdaEntryToJs(entry.rawValue.split(','));
+	}
+
+	const serializeSuffix = entry.rawName.split(c.SERIALIZE_FUNC_SUFFIX)[1];
+	const children: Array<Entry> = entry.rawValue === ''
+		? entry.children
+		: [{
+			rawName: '',
+			rawValue: entry.rawValue,
+			children: [],
+		}];
+	// eslint-disable-next-line no-use-before-define
+	const jsGroups = paramsToJsGroups(children, serializeSuffix);
+
+	if (entryType === EntryType.ConstantDef) {
+		return `const ${c.VAR_NAME_PREFIX}${funcName} = ${jsGroups[0]}`;
+	}
+
+	return `${funcName}(${jsGroups.join(', ')})`;
 }
 
 /**
@@ -207,25 +226,10 @@ export function paramsToJsGroups(params: Array<Entry>, serializSuffix?: string):
 	return groups
 		.map((group, id) => (group
 			.map((param) => (
-				// eslint-disable-next-line no-use-before-define
 				[id, -2].includes(serializedParamId) ? serialize(param) : entryToJs(param)
 			))
 			.join('.')
 		));
-}
-
-/**
- * Convert a Jaffle list into JavaScript code.
- * @param list the list to convert
- * @returns a string of JavaScript code which declares an array containing the list elements
- */
-export function listEntryToJs(list: Entry): string {
-	// eslint-disable-next-line no-use-before-define
-	return `[${list.children.map((item) => entryToJs(item)).join(', ')}]`;
-}
-
-export function entryToJs(entry: Entry): string {
-	return entry.rawName === '' ? rawValueToJs(entry.rawValue) : functionEntryToJs(entry);
 }
 
 export function rootEntryToJs(entry: Entry): string {
