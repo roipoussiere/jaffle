80c39bb1c1b3dca5e3b80161608287142f395cf2
Roipoussiere <roipoussiere@protonmail.com>
2023-08-11 11:19:16 +0200

    improve index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import Editor from './ui/ui';
-import YamlEditor from './ui/editors/yamlEditor';
-import NodeEditor from './ui/editors/nodeEditor';
+import { Editor, PlayButton, StopButton, WebsiteButton } from './ui/ui';
 
-import * as errors from './errors';
+import NodeEditor from './ui/editors/nodeEditor';
+import YamlEditor from './ui/editors/yamlEditor';
+import JsEditor from './ui/editors/jsEditor';
 
 import boxToEntry from './transpilers/graph/graphImporter';
 import yamlToEntry from './transpilers/yaml/yamlImporter';
@@ -10,14 +10,21 @@ import yamlToEntry from './transpilers/yaml/yamlImporter';
 import entryToBox from './transpilers/graph/graphExporter';
 import entryToJs from './transpilers/js/jsExporter';
 import entryToYaml from './transpilers/yaml/yamlExporter';
+
+import * as errors from './errors';
 import { entryToString } from './transpilers/utils';
 import { boxToString } from './transpilers/graph/graphUtils';
 
 export {
 	Editor,
+	JsEditor,
 	YamlEditor,
 	NodeEditor,
 	errors,
+
+	PlayButton,
+	StopButton,
+	WebsiteButton,
 
 	boxToEntry,
 	yamlToEntry,
