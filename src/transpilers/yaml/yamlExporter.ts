33a7f521ccdfe419b7535c283e7ce2cc9c85d030
Roipoussiere <roipoussiere@protonmail.com>
2023-08-14 12:12:41 +0200

    yamlExporter: dump value instead key/value when raw name is empty

--- src/transpilers/yaml/yamlExporter.ts
+++ src/transpilers/yaml/yamlExporter.ts
@@ -3,15 +3,15 @@ import { dump as dumpYaml } from 'js-yaml';
 
 import { Dict, Entry } from '../../model';
 
-export function entryToObject(entry: Entry): Dict<unknown> {
-	return {
-		[entry.rawName]: entry.children.length === 0
-			? entry.rawValue : entry.children.map((child) => entryToObject(child)),
-	};
+export function entryToObject(entry: Entry): unknown {
+	const value = entry.children.length === 0
+		? entry.rawValue : entry.children.map((child) => entryToObject(child));
+
+	return entry.rawName === '' ? value : { [entry.rawName]: value };
 }
 
 export function entryToYaml(entry: Entry): string {
-	const object = entryToObject(entry);
+	const object = entryToObject(entry) as Dict<unknown>;
 	return dumpYaml(object.root);
 }
 
