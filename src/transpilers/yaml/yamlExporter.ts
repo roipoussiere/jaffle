17704d361be136c203ade82b66faff7c5587d372
Roipoussiere <roipoussiere@protonmail.com>
2023-08-17 12:23:01 +0200

    update yaml exporter to use new serialized dict representation

--- src/transpilers/yaml/yamlExporter.ts
+++ src/transpilers/yaml/yamlExporter.ts
@@ -1,11 +1,23 @@
 // eslint-disable-next-line max-classes-per-file
 import { dump as dumpYaml } from 'js-yaml';
 
+import * as c from '../../constants';
 import { Dict, Entry } from '../../model';
 
 export function entryToObject(entry: Entry): unknown {
-	const value = entry.children.length === 0
-		? entry.rawValue : entry.children.map((child) => entryToObject(child));
+	let value: unknown;
+
+	if (entry.children.length === 0) {
+		value = entry.rawValue;
+	} else if (entry.children[0].rawName[0] === c.DICT_PREFIX) {
+		value = {};
+		entry.children.forEach((child) => {
+			const newKey = child.rawName.split(c.DICT_PREFIX).reverse()[0];
+			(value as Dict<unknown>)[newKey] = child.rawValue;
+		});
+	} else {
+		value = entry.children.map((child) => entryToObject(child));
+	}
 
 	return entry.rawName === '' ? value : { [entry.rawName]: value };
 }
