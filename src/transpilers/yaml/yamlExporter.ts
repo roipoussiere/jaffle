b7de3eb3507b0847c936c47bc9899bdb2afdb9d1
Roipoussiere <roipoussiere@protonmail.com>
2023-08-17 15:41:52 +0200

    fix yaml exporter when using serialized dict

--- src/transpilers/yaml/yamlExporter.ts
+++ src/transpilers/yaml/yamlExporter.ts
@@ -4,17 +4,31 @@ import { dump as dumpYaml } from 'js-yaml';
 import * as c from '../../constants';
 import { Dict, Entry } from '../../model';
 
+function dictEntryToDict(entry: Entry): Dict<unknown> {
+	const dict = {};
+	entry.children.forEach((child) => {
+		const newKey = child.rawName.split(c.DICT_PREFIX).reverse()[0];
+		let value: unknown;
+		if (child.children.length === 0) {
+			value = child.rawValue;
+		} else if (child.children[0].rawName[0] === c.DICT_PREFIX) {
+			value = dictEntryToDict(child);
+		} else {
+			// eslint-disable-next-line no-use-before-define
+			value = child.children.map((_child) => entryToObject(_child));
+		}
+		dict[newKey] = value;
+	});
+	return dict;
+}
+
 export function entryToObject(entry: Entry): unknown {
 	let value: unknown;
 
 	if (entry.children.length === 0) {
 		value = entry.rawValue;
 	} else if (entry.children[0].rawName[0] === c.DICT_PREFIX) {
-		value = {};
-		entry.children.forEach((child) => {
-			const newKey = child.rawName.split(c.DICT_PREFIX).reverse()[0];
-			(value as Dict<unknown>)[newKey] = child.rawValue;
-		});
+		value = dictEntryToDict(entry);
 	} else {
 		value = entry.children.map((child) => entryToObject(child));
 	}
