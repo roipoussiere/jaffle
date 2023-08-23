b9a9a94f29f5fedae4481f2c5b5f366e935fc1b8
Roipoussiere <roipoussiere@protonmail.com>
2023-08-23 19:07:12 +0200

    remove old unused errors, update errors tests

--- tests/errors.test.ts
+++ tests/errors.test.ts
@@ -3,16 +3,11 @@ import { describe, expect, test } from '@jest/globals';
 import * as e from '../src/errors';
 
 describe('Testing errors', () => {
-	test('Errors should throw when throwed', () => {
+	test('errors should raise when throwed', () => {
 		expect(() => { throw new e.JaffleError(''); }).toThrow(e.JaffleError);
 		expect(() => { throw new e.NotImplementedError(''); }).toThrow(e.NotImplementedError);
+		expect(() => { throw new e.UndefError(''); }).toThrow(e.UndefError);
 		expect(() => { throw new e.ImporterError(''); }).toThrow(e.ImporterError);
 		expect(() => { throw new e.ExporterError(''); }).toThrow(e.ExporterError);
-
-		expect(() => { throw new e.BadYamlJaffleError(''); }).toThrow(e.BadYamlJaffleError);
-		expect(() => { throw new e.BadStringJaffleError(''); }).toThrow(e.BadStringJaffleError);
-		expect(() => { throw new e.BadFunctionJaffleError(''); }).toThrow(e.BadFunctionJaffleError);
-		expect(() => { throw new e.BadListJaffleError(''); }).toThrow(e.BadListJaffleError);
-		expect(() => { throw new e.BadDocumentJaffleError(''); }).toThrow(e.BadDocumentJaffleError);
 	});
 });
